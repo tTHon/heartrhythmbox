ce18f7ed188e4dfd599f2f191846723c72d28f88
HeartRhythmBox <38881049+HeartRhythmBox@users.noreply.github.com>
2021-04-26 10:15:44 +0700

    new

---
+++ GdLines2019/block navigation.js
@@ -0,0 +1,56 @@
+/*call in the main page
+//up arrow
+window.addEventListener("keydown", function(event) {
+    event.preventDefault();
+    if (event.keyCode===38) {
+    blockNav(-1)}
+})
+//down arrow
+window.addEventListener("keydown", function(event) {
+    event.preventDefault();
+    if (event.keyCode===40) {
+    blockNav(1)}
+})
+*/
+
+
+function blockNav(n){
+    var block = document.getElementsByClassName("block");
+    var i;var blockOn=false;var largeBlock = false;
+    var screenTop = window.pageYOffset; var screenBottom = window.pageYOffset + window.innerHeight;
+    for (i=0;i<block.length;i++){
+        
+        if (blockOn) {break;}
+        if (block[i].offsetHeight>window.innerHeight){largeBlock==true}
+        var blockTop = block[i].offsetTop;
+        var blockBottom = block[i].offsetTop + block[i].offsetHeight;
+
+        //top and bottom edges are on screen//
+        if (blockTop>=screenTop && blockBottom<=screenBottom){
+            blockOn==true;var blockNo=i;
+            }
+        //top on screen; bottom - not yet//
+        else if (largeBlock =true && blockTop < screenTop + (0.5*window.innerHeight) &&
+            blockBottom>screenBottom) {
+            blockOn==true;var blockNo=i;
+            }
+        else if (largeBlock = true && blockTop < screenTop &&
+            blockBottom > screenTop + (0.5*window.innerHeight) ){
+            blockOn==true;var blockNo=i;
+            }
+    }
+    var nav = Number(n) + blockNo;
+    if (nav>=0 && nav<block.length){
+        next(nav);
+    }
+    
+}
+
+//nextBlk
+function next(n) {
+    var block = document.getElementsByClassName("block");
+    block[n].scrollIntoView({behavior:"smooth", block:"start"});
+}
+
+
+
---
+++ GdLines2019/wheel SUPERTHON 5_9_2019 10_01_11 PM.js
@@ -0,0 +1,60 @@
+//input here
+var allText = ["In symptomatic SND, pacemaker improves symptoms; not mortality",
+                "In a patient with symptomatic sinus bradycardia from guideline-directed medication, pacemaker is indicated.",
+                "In symptomatic SND, atrial-based pacing (not ventricular based or VVI) is recommended.",
+                "Symptomatic high-grade AV block + stable AV node blockers --> proceed with pacemaker without further observation."
+            ];
+var bg = ['green','green','green','yellow']
+
+//functions
+var tStart = 0;
+//tStart is a global variable = where the wheel is.
+function showTxtBlk(tInit,tEnd){
+    var listNo=0; var txtIndex;
+    var tMax=allText.length; 
+    
+    for (i=tInit; i<=tEnd; i++){
+      var txt = document.getElementsByClassName("card"+listNo);
+      if (i>=tMax){
+        txtIndex=i-tMax;
+      } else {txtIndex=i}
+      document.getElementsByClassName('card1')[0].classList.add('animateLeft10')
+      txt[0].innerHTML = allText[txtIndex];
+      
+      listNo++;
+      if (listNo>2) {listNo=0};
+    } 
+}
+
+function fwd() {
+    ++tStart;
+    if (tStart>=allText.length){tStart=0}
+    var tn=tStart+2;
+    showTxtBlk(tStart,tn);
+      }
+    
+function rw() {
+    --tStart;
+    if (tStart<0){tStart=(allText.length)-1}
+    var tn=tStart+2;
+    showTxtBlk(tStart,tn);
+    }   
+
+var init;
+var fwdCount=0;
+//fwdCount enables autorolling toggle.
+function autoFwd(){
+    if (fwdCount>0)
+    { fwdCount=0;
+      clearInterval(init)}
+    else  {  
+    ++fwdCount;
+    init = setInterval(function(){
+    ++tStart;
+    if (tStart>=allText.length){tStart=0}
+    var tn=tStart+2;
+    showTxtBlk(tStart,tn);
+    } , 1000);}
+    
+}
+    
