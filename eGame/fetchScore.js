a09c15c5709390a5464fc1c5805974e0e69ce5c8
tThon <79501326+tTHon@users.noreply.github.com>
2022-10-12 16:01:42 +0700

    up

--- eGame/fetchScore.js
+++ eGame/fetchScore.js
@@ -6,7 +6,7 @@ var p2Score = [];
 var p3Score = [];
 var questionNumber=0;
 
-function reFetch(questNo,gNo){ 
+function reFetch(){ 
   const options = {
     method: 'GET',
     headers: {
@@ -26,64 +26,57 @@ function reFetch(questNo,gNo){
       p1Score[f.qNo] = parseInt(f.p1);
       p2Score[f.qNo] = parseInt(f.p2);
       p3Score[f.qNo] = parseInt(f.p3);
-      var oldScore = [];var nowScore = [];
-
-      var score0=0;
-        for (let i=0;i<p1Score.length;i++){
-          score0 = score0 + p1Score[i];
-        }
-      
-      var score1=0;
-        for (let i=0;i<p2Score.length;i++){
-          score1 = score1 + p2Score[i];
-        }
-      
-      var score2=0;
-        for (let i=0;i<p3Score.length;i++){
-          score2 = score2 + p3Score[i];
-        }
-      }
-
-      nowScore[0]=score0;nowScore[1]=score1;          
-      nowScore[2]=score2;  
-      if (questNo==0) {
-        oldScore[0]=0;
-        oldScore[1]=0;
-        oldScore[2]=0;
-      }
-      else {
-        oldScore[0]=p1Score[questNo];
-        oldScore[1]=p2Score[questNo];
-        oldScore[2]=p3Score[questNo];   
-      }  
-      add = nowScore[gNo-1]-oldScore[gNo-1] 
-      //-120 to 420 degree -- move specific gNo
-      //1 score equal to how many degree
-
-      var block = 300/((totalQ-1)*10);
-      const degreeS = -120-((oldScore[gNo-1]*block));
-      const degreeE = -120-(nowScore[gNo-1]*block)
-      moveHand(gNo,degreeS,degreeE);
-      dropText(add,gNo);
-      showScore(gNo,(oldScore[gNo-1]),nowScore[gNo-1])
+    }
   }
 }
 
-function next(){
+function updateScore(gNo,array,qNo){
+  if (qNo==0) {oldScore=0;
+  newScore = array[0]}
+  else if (qNo>0) {
+    oldScore = sumScore(array,qNo-1);
+    newScore = sumScore(array,qNo)
+  }
+
+ 
+  function sumScore(array,q){
+    var sum=0;
+    for (let index = 0; index <= q; index++) {
+      if (array[index]>=0)
+        {sum = sum + array[index]}
+     }
+    return sum
+  }
+
+  const block = 300/((totalQ-1)*10);
+  const degreeS = -120-(oldScore*block);
+  const degreeE = -120-(newScore*block)
+  const add = newScore-oldScore
+  moveHand(gNo,degreeS,degreeE);
+  dropText(add,gNo);
+  showScore(gNo,oldScore,newScore)
+}
+
+function next(dir){
   qText = document.getElementById('qNow')
   qMax = totalQ-1
-  questionNumber ++;
-  
+  questionNumber = questionNumber+dir;
+  if (questionNumber<0) {questionNumber=0}
   qText.innerHTML = questionNumber
   document.getElementById('questionNo').value = questionNumber;
       //document.getElementById('submitBut').click();
   if (questionNumber==qMax){
   document.getElementById('nextArrow').style.display='none'}
+  else {document.getElementById('bkArrow').style.display='inline'}
+  if (questionNumber==0){
+    document.getElementById('bkArrow').style.display='none'}
+  else {document.getElementById('bkArrow').style.display='inline'}
 
   
 }
 
 function showScore(gNo,oldS,newS){
+
   //showScore
   const board = document.getElementsByClassName('score')
   var add = newS-oldS;
