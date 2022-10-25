b652510866055b3eacb08d8f95f42dee0ac87389
tThon <79501326+tTHon@users.noreply.github.com>
2022-10-25 10:48:06 +0700

    bb

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,11 @@
 exports.handler = async function (event, context) {
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+  
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello World" }),
+      body: JSON.stringify(myForm)
+      //body: JSON.stringify({ message: "Hello World" }),
     };
   };
