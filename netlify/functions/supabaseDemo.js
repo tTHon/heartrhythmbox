f8673aafc43c9fcbba4f767c6a1b15c11a2d1fbe
HeartRhythmBox <sirin_md@hotmail.com>
2022-10-25 19:38:22 +0700

    Update supabaseDemo.js

--- netlify/functions/supabaseDemo.js
+++ netlify/functions/supabaseDemo.js
@@ -1,7 +1,7 @@
 //sbp_73eaa8e734d970f52610cb46fd1a4d6d7c08a19d
 //npm install --save @supabase/supabase-js
 
-const SUPABASE_KEY = 'sbp_73eaa8e734d970f52610cb46fd1a4d6d7c08a19d'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vb3NwbWNnamFtdnBneGxnbXljIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NjY5Nzk2MCwiZXhwIjoxOTgyMjczOTYwfQ.oy4V-23uYXch0gs6iIbGPrvIj40RlgcTKBLxf4m9L8s'
 const supabaseKey = process.env.SUPABASE_KEY
 const SUPABASE_URL = "https://noospmcgjamvpgxlgmyc.supabase.co"
 
