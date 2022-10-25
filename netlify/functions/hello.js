f53c4d75362daf477f7940d0b62882e28adc201b
tThon <79501326+tTHon@users.noreply.github.com>
2022-10-25 11:00:11 +0700

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -5,7 +5,7 @@ exports.handler = async function (event, context) {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(name)
+      body: JSON.stringify({name})
       //body: JSON.stringify({ message: "Hello World" }),
     };
   };
