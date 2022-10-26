ce120128dde834d8d40f0d2c9242e48dc8295738
tThon <79501326+tTHon@users.noreply.github.com>
2022-10-26 09:27:42 +0700

    bb

--- netlify/functions/supabaseDemo.js
+++ netlify/functions/supabaseDemo.js
@@ -1,14 +1,7 @@
-// Grab our credentials from a .env file or environment variables
-require('dotenv').config();
-
-const {
-    SUPABASE_URL,
-    SESSION_SECRET
-} = process.env;
+import { createClient } from '@supabase/supabase-js'
 
 // Connect to our database 
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(SUPABASE_URL, SESSION_SECRET);
+const _supabase = createClient('https://noospmcgjamvpgxlgmyc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vb3NwbWNnamFtdnBneGxnbXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY2OTc5NjAsImV4cCI6MTk4MjI3Mzk2MH0.qbIQW8O_5mm5Dbz5_GJIBQE1fGo5PWM-xhDqeMWcGuY')
 
 // Our standard serverless handler function
 exports.handler = async event => {
