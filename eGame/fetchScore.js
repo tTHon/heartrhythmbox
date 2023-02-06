b89c04951a2b0ae768719a2af194fcae6ae1a513
tThon <79501326+tTHon@users.noreply.github.com>
2023-02-07 06:33:50 +0700

    Update fetchScore.js

--- eGame/fetchScore.js
+++ eGame/fetchScore.js
@@ -614,8 +614,8 @@ function drawLabel(gNo){
   var baseX = (16*vw*(2*gNo-1)); 
   max = (totalQ-1)*10
   //cos 62
-  var xLeft=baseX-(Math.cos(Math.PI*55/180)*(radius))-(1.51*vw);
-  var xRight = baseX+(Math.cos(Math.PI*55/180)*(radius))+(1.51*vw);
+  var xLeft=baseX-(Math.cos(Math.PI*55/180)*(radius))-(1.55*vw);
+  var xRight = baseX+(Math.cos(Math.PI*55/180)*(radius))+(1.55*vw);
   var yPos = baseY+(radius);
   ctx.beginPath();
   ctx.fillStyle = "#ddd"
