27c9e14775407749ac35cf24d55b5d7c8e4e8db5
tThon <79501326+tTHon@users.noreply.github.com>
2023-04-25 10:13:08 +0700

    c

--- playground/program.js
+++ playground/program.js
@@ -2,6 +2,7 @@ function addTopic(array){
     console.log(array)
     //one hour equal to 3vh + header of 9vh
     const oneHour = 9*vh;
+    console.log('oneHour = ' + oneHour)
     const dayTable = document.getElementById('dayTable');
     
     //mins display
@@ -10,11 +11,19 @@ function addTopic(array){
     var endHour = Number(array.endTime.slice(0,2))
     var endMin = Number(array.endTime.slice(3,5))
     //find starting hour row
-    const startRow = dayTable.rows[startHour].cells[1]
+    const startRow = dayTable.rows[startHour+1].cells[1]
     const newBar = document.createElement('div')
     startRow.appendChild(newBar)
-    newBar.style.height = oneHour*(startMin/60) + 'px'
-    console.log(newBar.style.height)
+    var top = oneHour*(startMin/60)
+    newBar.style.top = top + 'px'
+    console.log(newBar.style.top)
+    //calculate bar height in minutes
+    var barMin = (60*endHour+endMin)-(60*startHour+startMin)
+    var height = oneHour*(barMin/60)
+    console.log('barMin = '+barMin)
+    newBar.style.height = height + 'px'
+    console.log('height = ' + height)
+    //calculate bar top
     newBar.className = 'topicBar'
     newBar.innerHTML = array.title
 }
