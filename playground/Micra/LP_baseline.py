afda370018e4e61ba5cad5f16ebe684439676cd2
tThon <79501326+tTHon@users.noreply.github.com>
2025-09-22 13:31:25 +0700

    up

--- playground/Micra/LP_baseline.py
+++ playground/Micra/LP_baseline.py
@@ -10,20 +10,24 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
-#print(df)
+# Convert T2FU from days to months
+df['T2FU_months'] = df['T2FU'] / 30.44  # 30.44 is the average number of days in a month
+
 
 def summarize_continuous(series):
-    return f"{series.mean():.1f} ± {series.std():.1f}"
+    return f"{series.mean():.1f} ± {series.std():.1f} ({series.min():.1f}-{series.max():.1f})"
 
 def summarize_categorical(series):
     counts = series.value_counts(dropna=False)
     total = len(series)
     return "; ".join([f"{idx}: {val} ({val/total*100:.1f}%)" for idx, val in counts.items()])
 
-continuous_vars = ["Age", "Weight", "Height", "BSAYu","CCI","T2FU","CKD"]
-categorical_vars = ["Sex", "MI","PCI/CABG","CHF","PAD","CVA","Dementia","COPD","CNT","PU","Liver","DM","CKD","Malignancy","TV","SigValve","AF","HTN","CCISev","Antiplatelet","OAC","IndicationPPM","IndicationforMicra","Access","Position","Model","Hemostasis"] 
+#continuous_vars = ["Age", "Weight", "Height", "BSAYu","CCI","T2FU","CKD"]
+continuous_vars = ["T2FU_months"]
+#categorical_vars = ["Sex", "MI","PCI/CABG","CHF","PAD","CVA","Dementia","COPD","CNT","PU","Liver","DM","CKD","Malignancy","TV","SigValve","AF","HTN","CCISev","Antiplatelet","OAC","Access","Position","Model","Hemostasis"] 
 
-#categorical_vars = ["CKD","CCISev","DM"] 
+#categorical_vars = ["IndicationPPM","IndicationforMicra"] 
+categorical_vars = ["AcuteCom","ChronicCom",'Death']
 # Overall
 summary_all = {}
 for var in continuous_vars:
@@ -38,4 +42,7 @@ summary_all_df = pd.DataFrame.from_dict(summary_all, orient="index", columns=["A
 
 # Final table
 baseline_table = summary_all_df
-print(baseline_table)
+#print(baseline_table)
+
+# Print the table in a markdown format
+print(baseline_table.to_markdown())
