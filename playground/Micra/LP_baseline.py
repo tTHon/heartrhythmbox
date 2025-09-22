d6d341491e2073bcf79d5afb9986a793b0142bda
tThon <79501326+tTHon@users.noreply.github.com>
2025-09-22 12:18:11 +0700

    up

---
+++ playground/Micra/LP_baseline.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+
+# Load your CSV
+df = pd.read_csv("playground/Micra/LPBaseline.csv")
+
+# Show all rows and columns
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+#print(df)
+
+def summarize_continuous(series):
+    return f"{series.mean():.1f} ± {series.std():.1f}"
+
+def summarize_categorical(series):
+    counts = series.value_counts(dropna=False)
+    total = len(series)
+    return "; ".join([f"{idx}: {val} ({val/total*100:.1f}%)" for idx, val in counts.items()])
+
+continuous_vars = ["Age", "Weight", "Height", "BSAYu","CCI","T2FU","CKD"]
+categorical_vars = ["Sex", "MI","PCI/CABG","CHF","PAD","CVA","Dementia","COPD","CNT","PU","Liver","DM","CKD","Malignancy","TV","SigValve","AF","HTN","CCISev","Antiplatelet","OAC","IndicationPPM","IndicationforMicra","Access","Position","Model","Hemostasis"] 
+
+#categorical_vars = ["CKD","CCISev","DM"] 
+# Overall
+summary_all = {}
+for var in continuous_vars:
+    if var in df.columns:
+        summary_all[var] = summarize_continuous(df[var].dropna())
+for var in categorical_vars:
+    if var in df.columns:
+        summary_all[var] = summarize_categorical(df[var].dropna())
+
+num_patients = len(df)
+summary_all_df = pd.DataFrame.from_dict(summary_all, orient="index", columns=["All Patients"+f' (N={num_patients})'])
+
+# Final table
+baseline_table = summary_all_df
+print(baseline_table)
