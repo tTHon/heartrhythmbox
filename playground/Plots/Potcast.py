dd4b64c944951c0fdc5559ac6c7884c570b3476b
tThon <79501326+tTHon@users.noreply.github.com>
2026-06-07 13:32:54 +0700

    h

--- playground/Plots/Potcast.py
+++ playground/Plots/Potcast.py
@@ -2,38 +2,42 @@ import matplotlib.pyplot as plt
 
 
 labels = [
-    "PREVAIL & \nPROTECT-AF",
-    "PRAGUE-17",
-    "OPTION",
-    "Total"
+    "Hemorrhagic Stroke",
+    "All Major Bleeding \n(Procedural & Non-Procedural)",
+    "Non-procedural Related \nMajor Bleeding",
+    "All major bleeding \n+ CRNMB",
+    "Non-procedural Related \nMajor Bleeding + CRNMB"
 ]
 
 # Incidence of ischemic stroke/se Data: (n, %)
-# LAAO
+# OAC
 group_intervention = [
-    ('45/732', 6.1),
-    ('13/201', 6.5),
-    ('11/803', 1.4),
-    ('69/1736', 4.0)
+    (5, 0.4),
+    (87, 6.4),
+    (87, 6.4),
+    (260, 19.0),
+    (260, 19.0)
 ]
 
-# Group 2: OAC
+# Group 2: LAAO
 group_control = [
-    ('14/382', 3.7),
-    ('10/201', 5.0),
-    ('11/797', 1.4),
-    ('35/1580', 2.2)
+    (5, 0.4),
+    (83, 5.9),
+    (71, 5.1),
+    (181, 12.8),
+    (154, 10.9)
 ]
 
 # Hazard Ratios
-hrs = [1.68, 1.30, 0.99, 1.38]
+hrs = [0.96, 0.92, 0.78, 0.66,0.55]
 
 # 95% Confidence Intervals (Lower, Upper)
 cis = [
-    (0.93, 3.02),
-    (0.58, 2.90),
-    (0.43, 2.28),
-    (0.91, 2.08)
+    (0.28, 3.32),
+    (0.68, 1.24),
+    (0.57, 1.06),
+    (0.54, 0.80),
+    (0.45, 0.67)
 ]
 
 # Calculate error bar lengths
@@ -48,12 +52,12 @@ plt.rcParams.update({'font.size': font_size, 'font.family': 'inter'})
 # Colors suitable for dark blue background
 bg_color = 'none' # DarkBlue
 text_color = 'white'
-marker_color = '#FED154' # DeepSkyBlue
+marker_color = "#F26868" # DeepSkyBlue
 line_color = '#eeeeea'
 grid_color = 'gray'
 
 # Create the plot
-fig, ax = plt.subplots(figsize=(40, 18), dpi=300)
+fig, ax = plt.subplots(figsize=(44, 20), dpi=300)
 fig.patch.set_facecolor(bg_color)
 ax.set_facecolor(bg_color)
 
@@ -63,9 +67,9 @@ y_pos = range(len(labels))
 ax.errorbar(hrs, y_pos, xerr=errors, fmt='o', markersize=45, color=marker_color, ecolor=line_color, capsize=20, linewidth=15, capthick=5)
 
 # Add a vertical line at HR = 1
-ax.axvline(x=1, color=grid_color, linestyle='--', linewidth=3)
+ax.axvline(x=1, color=grid_color, linestyle='--', linewidth=5)
 #ax.axvline(x=0.5, color=grid_color, linestyle='--', linewidth=2, alpha=0.8)
-ax.axvline(x=3, color=grid_color, linestyle='--', linewidth=2, alpha=0.8)
+#ax.axvline(x=3, color=grid_color, linestyle='--', linewidth=2, alpha=0.8)
 
 # Remove default y-axis labels
 ax.set_yticks([]) 
@@ -74,12 +78,12 @@ ax.set_xlim(0,4.0)
 
 # Add column headers with white text
 header_y = -0.7
-ax.text(-1, header_y, "Trials", fontweight='bold', ha='left', color=text_color)
+#ax.text(-1, header_y, "Trials", fontweight='bold', ha='left', color=text_color)
 
 #ax.text(-3, header_y, "Trials", fontweight='bold', ha='left', color=text_color)
-#ax.text(-1.2, header_y, "LAAO (n/N, %)", fontweight='bold', ha='center', color=text_color)
-#ax.text(-0.1, header_y, "OAC (n/N, %)", fontweight='bold', ha='center', color=text_color)
-ax.text(3.7, header_y, "Hazard Ratio (95% CI)", fontweight='bold', ha='center', color=text_color)
+ax.text(-1.4, header_y, "OAC (n, %)", fontweight='bold', ha='center', color=text_color)
+ax.text(-0.4, header_y, "LAAO (n, %)", fontweight='bold', ha='center', color=text_color)
+ax.text(4.0, header_y, "Hazard Ratio (95% CI)", fontweight='bold', ha='center', color=text_color)
 
 
 # Add data rows
@@ -88,24 +92,24 @@ for i in range(len(labels)):
     
     # Endpoint Label
     #ax.text(-3, y, labels[i], va='center', ha='left', color=text_color, fontweight='bold', fontsize=font_size+6)
-    ax.text(-1, y, labels[i], va='center', ha='left', color=text_color, fontweight='bold', fontsize=font_size+6)
+    ax.text(-4, y, labels[i], va='center', ha='left', color=text_color, fontweight='bold', fontsize=font_size+6)
     
     # Intervention Data
-    #n_int, p_int = group_intervention[i]
-    #ax.text(-1.2, y, f"{n_int} ({p_int}%)", va='center', ha='center', color=text_color)
+    n_int, p_int = group_intervention[i]
+    ax.text(-1.4, y, f"{n_int} ({p_int}%)", va='center', ha='center', color=text_color)
     
     # Control Data
-    #n_ctrl, p_ctrl = group_control[i]
-    #ax.text(-0.1, y, f"{n_ctrl} ({p_ctrl}%)", va='center', ha='center', color=text_color)
+    n_ctrl, p_ctrl = group_control[i]
+    ax.text(-0.4, y, f"{n_ctrl} ({p_ctrl}%)", va='center', ha='center', color=text_color)
     
     # HR text
     hr, ci = hrs[i], cis[i]
     hr_text = f"{hr:.2f} ({ci[0]:.2f}-{ci[1]:.2f})"
-    ax.text(3.7, y, hr_text, va='center', ha='center', color=text_color)
+    ax.text(4.0, y, hr_text, va='center', ha='center', color=text_color)
 
 # Formatting
 ax.invert_yaxis()  # Primary endpoint at the top
-ax.set_xlabel('Hazard Ratio (log scale) of LAAO vs OAC', fontsize=font_size, labelpad=20, color=text_color)
+ax.set_xlabel('Hazard Ratio (log scale) of LAAO vs OAC', fontsize=font_size, labelpad=8, color=text_color)
 #ax.set_title('Primary Endpoint and Components (POTCAST Trial)', fontsize=font_size+4, pad=40, fontweight='bold', color=text_color)
 
 # Customize spines
